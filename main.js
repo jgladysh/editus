cd50054bdcda630cf5357ebc594dd9dfab68046a
JGladysh <jgladysh@gmail.com>
2015-07-14 16:19:15 +0300

    - Added hot keys functionality - Provide undo/redo functionality - Handle changes in highlighted elements

--- main.js
+++ main.js
@@ -2,17 +2,103 @@
  * Created by julia on 6/22/15.
  */
 
-var node = '',
-    content,
-    wrapper,
-    keyWordsArray = ['create', 'experiment', 'assign', 'to', 'all', 'users', 'where', 'for', 'salt', 'new'];
+var content,
+    keyWordsArray = ["create", "experiment", "assign", "to", "all", "users", "where", "for", "salt", "new"],
+    canRedo,
+    canUndo,
+    wasUndo,
+    EditCommand,
+    startValue,
+    newValue,
+    timer,
+    cursorChange = false,
+    position = 0,
+    meta = false,
+    stack = new Undo.Stack(),
+    blocked = false;
 
 var suggestions = $.parseHTML('<div class="list-group"><a href="#" class="list-group-item">Item 1</a><a href="#" class="list-group-item">Item 2</a> <a href="#" class="list-group-item">Item 3</a> <a href="#" class="list-group-item">Item 4</a> <a href="#" class="list-group-item">Item 5</a> </div>')[0];
 
 $(document).ready(function () {
     content = $('#content')[0];
-    wrapper = $('#wrapper')[0];
+    startValue = content.innerHTML;
+
+    EditCommand = Undo.Command.extend({
+        constructor: function (textarea, oldValue, newValue, undoPosition, redoPosition) {
+            this.textarea = textarea;
+            this.oldValue = oldValue;
+            this.newValue = newValue;
+            this.undoPosition = undoPosition;
+            this.redoPosition = redoPosition;
+
+        },
+        execute: function () {
+        },
+        undo: function () {
+            blocked = true;
+            this.textarea.innerHTML = this.oldValue;
+            startValue = content.innerHTML;
+            setCaretCharIndex(content, this.undoPosition);
+            wasUndo = true;
+        },
+
+        redo: function () {
+            blocked = true;
+            this.textarea.innerHTML = this.newValue;
+            startValue = content.innerHTML;
+            setCaretCharIndex(content, this.redoPosition);
+        }
+    });
+
+    function stackUI() {
+        canRedo = stack.canRedo();
+        canUndo = stack.canUndo();
+    }
+
+    stackUI();
+    stack.changed = function () {
+        stackUI();
+    };
 });
+
+var execute = function (timeout) {
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+        var range = window.getSelection().getRangeAt(0),
+            newValue = content.innerHTML;
+        if (meta) {
+            meta = false;
+            return;
+        }
+        var undoPosition = position,
+            redoPosition = getCharacterOffsetWithin(range, content);
+
+        if (undoPosition == redoPosition) {
+            return;
+        }
+        if (wasUndo) {
+            console.log(stack.commands[stack.stackPosition].undoPosition + '  ' + stack.commands[stack.stackPosition].redoPosition)
+            stack.execute(new EditCommand(content, startValue, newValue, stack.commands[stack.stackPosition].redoPosition, redoPosition));
+        }
+        else if (undoPosition != redoPosition && newValue == startValue) {
+            if (cursorChange) {
+                stack.commands[stack.commands.length - 1].redoPosition = redoPosition;
+            }
+            else {
+                cursorChange = true;
+                stack.execute(new EditCommand(content, startValue, newValue, undoPosition, redoPosition));
+            }
+
+        }
+        else {
+            cursorChange = false;
+            stack.execute(new EditCommand(content, startValue, newValue, undoPosition, redoPosition));
+        }
+        startValue = newValue;
+        position = redoPosition;
+        wasUndo = false;
+    }, timeout);
+};
 
 
 function suggest(e) {
@@ -23,42 +109,60 @@ function suggest(e) {
 
         initialisePopover(popover, popoverContainer, position.top + 25, position.left);
     }
+    else if (e.metaKey && e.keyCode == 90) {
+        meta = true;
+        e.preventDefault();
+        if (canUndo) {
+            stack.undo();
+            process();
+        }
+    }
+    else if (e.metaKey && e.keyCode == 89) {
+        meta = true;
+        e.preventDefault();
+        if (canRedo) {
+            stack.redo();
+            process();
+        }
+    }
+    else if (e.metaKey && e.keyCode == 65) {
+        meta = true;
+    }
 }
 
-
-function processOnChange(e) {
+var processOnChange = function (e) {
+    var d = new $.Deferred();
+    var sel = window.getSelection(),
+        nodeToCheck = sel.baseNode.parentElement;
+    if (sel.type == "Range") {
+        return d.resolve();
+    }
     if (e.keyCode == 32) {
+        if (nodeToCheck.className == 'highlighted') {
+            normalizeSpaceAtUndo(nodeToCheck, sel);
+        }
+        checkHighlighted();
         process();
-    } else if (e.keyCode == 13) {
+    }
+    else if (e.keyCode == 13) {
         if (content.firstChild != null && document.createRange) {
+            checkHighlighted();
             checkEveryTag(content);
         }
     }
     else {
-        var sel = window.getSelection(),
-            anchorNode = sel.anchorNode,
-            nextNode = anchorNode.nextElementSibling,
-            nodeToCheck = sel.baseNode.parentElement;
-        if (anchorNode.length == sel.anchorOffset && (nextNode && nextNode.nodeName == 'SPAN')) {
-            $(nextNode).contents().unwrap();
-            content.normalize();
-        }
-        if (nodeToCheck.className == 'highlighted') {
-            var selection = window.getSelection(),
-                range = selection.getRangeAt(0),
-                char = getCharacterOffsetWithin(range, content);
-            checkHighlighted();
-            setCaretCharIndex(content, char);
-        }
+        checkHighlighted();
     }
-}
+    return d.promise();
+};
 
 
 function process() {
+    var d = new $.Deferred();
     if (content.firstChild != null) {
         var selection = window.getSelection();
         if (selection.type == "Range") {
-            return;
+            return d.resolve();
         }
         var range = selection.getRangeAt(0),
             char = getCharacterOffsetWithin(range, content);
@@ -67,6 +171,7 @@ function process() {
         }
         setCaretCharIndex(content, char);
     }
+    return d.promise();
 }
 
 
@@ -81,7 +186,6 @@ function checkEveryTag(node) {
     else {
         var ranges = makeRangesFromMatches(keyWordsArray, node);
         wrapNodes(ranges);
-        console.log(node.data)
     }
 }
 
@@ -195,13 +299,37 @@ function getCursorCoordinates() {
 
 
 function checkHighlighted() {
-    var highlighted = $('.highlighted');
-    for (var i = 0; i < highlighted.length; i++) {
-        if ($(highlighted[i]).text() != $.trim($(highlighted[i]).data('content'))) {
-            $(highlighted[i]).contents().unwrap();
-            content.normalize();
-        }
+    var sel = window.getSelection(),
+        anchorNode = sel.anchorNode,
+        nextNode = anchorNode.nextElementSibling,
+        nodeToCheck = sel.baseNode.parentElement;
+    if (anchorNode.length == sel.anchorOffset && (nextNode && nextNode.nodeName == 'SPAN')) {
+        $(nextNode).contents().unwrap();
+        content.normalize();
     }
+    if (nodeToCheck.className == 'highlighted') {
+        var range = sel.getRangeAt(0),
+            char = getCharacterOffsetWithin(range, content),
+            highlighted = $('.highlighted');
+        for (var i = 0; i < highlighted.length; i++) {
+            var text = $(highlighted[i]).text();
+            if (!(new RegExp(keyWordsArray.map(function (w) {
+                    return '\\b' + w + '\\b((?!\\W(?=\\w))|(?=\\s))'
+                }).join('|'), 'gi').test(text))) {
+                $(highlighted[i]).contents().unwrap();
+                content.normalize();
+            }
+        }
+        setCaretCharIndex(content, char);
+    }
+}
+
+function normalizeSpaceAtUndo(nodeToCheck, selection) {
+    var range = selection.getRangeAt(0),
+        char = getCharacterOffsetWithin(range, content);
+    $(nodeToCheck).contents().unwrap();
+    content.normalize();
+    setCaretCharIndex(content, char);
 }
 
 
