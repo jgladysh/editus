5c582c64aa5352fa5ff4952bcd4da7b8aa29aff1
JGladysh <jgladysh@gmail.com>
2015-09-17 18:06:54 -0700

    - Testing

--- spec/editus-spec.js
+++ spec/editus-spec.js
@@ -2,6 +2,7 @@ import {initEditus} from '../src/js/editus';
 
 describe("highlighting", function () {
     var editus,
+        span,
         div;
     beforeEach(function () {
         div = $("<div contenteditable='true' class = 'content' id='content3'> </div>");
@@ -14,14 +15,31 @@ describe("highlighting", function () {
     it("check that editor has specified id", function () {
         expect(editus.content().id).toEqual('content3');
     });
-    it("check that editor's highlighting function throw error without required arguments", function () {
+    it("check that editor's highlighting function throws error without required arguments", function () {
         expect(function(){editus.setHighlightingWords(['a','aaa','aa'])}).toThrow('Class name for highlighted words in String format should be provided');
         expect(function(){editus.setHighlightingWords('')}).toThrow('Array of highlighted words should be provided');
-
+    });
+    it("check that highlighted object was created and highlights were set", function(){
+        editus.setHighlightingWords(['a','aaa','aa'], 'highlighted');
+        expect(editus.Highlighting).toBeDefined();
+    });
+    it("check that editors suggestion function throws error without required arguments", function(){
+        expect(function(){editus.setSuggestionsService();}).toThrow('url to backend suggestion service should be provided');
+    });
+    it("check that editors suggestion function create appropriate variables", function(){
+        editus.setSuggestionsService('http://localhost:3000/');
+        expect(editus.popoverContainerId).toEqual('popoverContainer_content3');
+        expect(editus.popoverId).toEqual('popover_content3');
+        expect(editus.Suggestion).toBeDefined();
     });
     afterEach(function () {
         div.remove();
         div = null;
         editus = null;
+    });
+    it("check that editor throw error when element that passed isn't type of DIV", function(){
+        span = $("<span contenteditable='true' class = 'content' id='content4'> </span>");
+        $(document.body).append(span);
+        expect(function(){initEditus('content4')}).toThrow('Editable element must be DIV');
     });
 });
--- spec/highlighting-spec.js
+++ spec/highlighting-spec.js
@@ -1,12 +1,27 @@
-import {Highlighting} from '../src/js/highlighting';
+import {initEditus} from '../src/js/editus';
 
 describe("highlighting", function () {
-    var highlighting;
-    beforeEach(function() {
-        highlighting = new Highlighting;
+    var highlighting,
+        editus,
+        div;
+    beforeEach(function () {
+        div = $("<div contenteditable='true' class = 'content' id='content3'>to t</div>");
+        $(document.body).append(div);
+        editus = initEditus('content3');
+        editus.setHighlightingWords(['to', 'all', 'users'], 'highlighted');
+        highlighting = editus.Highlighting;
     });
     it("returns the position of word in text", function () {
-        var matches = highlighting.getMatches('to','ti to');
+        var matches = highlighting.getMatches('to', 'ti to');
         expect(matches[0]).toBe(3);
+    });
+    it("check that node was wrapped", function () {
+        highlighting.checkEveryTag(editus.content());
+        expect(editus.content().childNodes[1].nodeName).toEqual('SPAN');
+    });
+    afterEach(function () {
+        div.remove();
+        div = null;
+        editus = null;
     });
 });
